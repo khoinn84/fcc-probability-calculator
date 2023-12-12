bc6f2d93ac088d68ad3c144d4d8b4f775b4b36bc
khoinn84 <36420485+khoinn84@users.noreply.github.com>
2023-12-12 23:40:47 +0700

    Add all needed functions
    This has passed 2/3 tests on FCC. The outstanding issue is the calculated probability doesn't match the expected probability from FCC test code.

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,12 +4,39 @@ import random
 
 class Hat:
     def __init__(self, **ballcolor):
+        self.contents = []
         for key, value in ballcolor.items():
-            if value != 1:
-                print(f"Color {key} has {value} balls")
-            else:
-                print(f"Color {key} has {value} ball")
+            for i in range(value):
+                self.contents.append(key)
 
+    def draw(self, num_balls_drawn):
+        if num_balls_drawn >= len(self.contents):
+            return self.contents
+        else:
+            balls_drawn = []
+            tempList = copy.deepcopy(self.contents)
+            for i in range(num_balls_drawn):
+                ball_drawn = random.choice(tempList)
+                balls_drawn.append(ball_drawn)
+                tempList.remove(ball_drawn)
+            #print("Ball drawn",balls_drawn)
+            #print("The rest",self.contents)
+            return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    return True
+    M = 0
+    expectation = []
+  
+    for key, value in expected_balls.items():
+        for i in range(value):
+            expectation.append(key)
+
+    for i in range(num_experiments):
+        hatcopy = copy.deepcopy(hat)
+        drawnset = hatcopy.draw(num_balls_drawn)
+        if set(expectation).issubset(set(drawnset)):
+            M += 1
+        
+
+    
+    return M / num_experiments
