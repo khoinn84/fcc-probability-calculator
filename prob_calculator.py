7733a8951948b07796ed3dfa9e0de9bbfb72b826
khoinn84 <36420485+khoinn84@users.noreply.github.com>
2023-12-14 01:52:28 +0700

    Fixed algorithm to check if drawn balls contain expected balls more precisely

--- prob_calculator.py
+++ prob_calculator.py
@@ -14,13 +14,14 @@ class Hat:
             return self.contents
         else:
             balls_drawn = []
-            tempList = copy.deepcopy(self.contents)
             for i in range(num_balls_drawn):
-                ball_drawn = random.choice(tempList)
+                ball_drawn = random.choice(self.contents)
                 balls_drawn.append(ball_drawn)
-                tempList.remove(ball_drawn)
-            #print("Ball drawn",balls_drawn)
-            #print("The rest",self.contents)
+                self.contents.remove(ball_drawn)
+                #print("Balls drawn",balls_drawn)
+                #print("The rest:\n",tempList)
+                #print("==============")
+            #print("Balls drawn",balls_drawn)
             return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
@@ -34,9 +35,23 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     for i in range(num_experiments):
         hatcopy = copy.deepcopy(hat)
         drawnset = hatcopy.draw(num_balls_drawn)
-        if set(expectation).issubset(set(drawnset)):
+        #if set(expectation).issubset(set(drawnset)):
+        #if all(element in drawnset for element in expectation):
+        if checkLWL(expectation,drawnset):
+            #print(expectation)
+            #print(drawnset)
             M += 1
-        
-
+            #print('M',M)
     
     return M / num_experiments
+
+def checkLWL(ls, lb):
+    listsmall = copy.deepcopy(ls)
+    listbig = copy.deepcopy(lb)
+    while len(listsmall) > 0:
+        if listsmall[0] not in listbig:
+            return False
+        else:
+            listbig.remove(listsmall[0])
+            listsmall.remove(listsmall[0])
+    return True
